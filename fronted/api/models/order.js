440bb8d4de55d477fe5e81687431c6bfe685c24f
sergio <stortosa01@gmail.com>
2020-06-16 09:08:04 +0200

    remove things

--- fronted/api/models/order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  goal: { type: mongoose.Schema.Types.ObjectId, ref: 'Goal', require: true },
-  quantity: { type: Number, default: 1 }
-});
-
-module.exports = mongoose.model('Order', orderSchema);
--- fronted/back/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const express = require('express');
-
-const port = process.env.PORT || 4000;   // or 3000
-
-const server = http.createServer(app);
-
-server.listen(port);
